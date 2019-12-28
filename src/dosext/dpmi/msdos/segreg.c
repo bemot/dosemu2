4b88c79a28f61b850887bc715e0ed9252ad4255f
Stas Sergeev <stsp@users.sourceforge.net>
2019-12-29 02:44:46 +0300

    Revert "msdos: remove dead code"
    This reverts commit 8acec250f0ccd8b3137b2a01e05f1dea043c9e56.

--- src/dosext/dpmi/msdos/segreg.c
+++ src/dosext/dpmi/msdos/segreg.c
@@ -47,8 +47,92 @@ static enum MfRet msdos_sel_fault(sigcontext_t *scp)
     unsigned short desc;
 
     D_printf("MSDOS: msdos_fault, err=%#x\n", _err);
-    if ((_err & 0xffff) == 0)	/*  not a selector error */
+    if ((_err & 0xffff) == 0) {	/*  not a selector error */
+    /* Why should we "fix" the NULL dereferences? */
+    /* Because the unmodified Win3.1 kernel (not WinOS2) needs this */
+    /* Yes, but only when LDT is read-only, and then it doesn't work anyway.
+     * So lets disable it again and see if someone else needs this. */
+#if 0
+	char fixed = MFR_NOT_HANDLED;
+	unsigned char * csp;
+
+	csp = (unsigned char *) SEL_ADR(_cs, _eip);
+
+	/* see if client wants to access control registers */
+	if (*csp == 0x0f) {
+	  if (cpu_trap_0f(csp, scp)) return 1;	/* 1=handled */
+	}
+
+	switch (*csp) {
+	case 0x2e:		/* cs: */
+	    break;		/* do nothing */
+	case 0x36:		/* ss: */
+	    break;		/* do nothing */
+	case 0x26:		/* es: */
+	    if (_es == 0) {
+		D_printf("MSDOS: client tries to use use gdt 0 as es\n");
+		_es = ConvertSegmentToDescriptor(0);
+		fixed = MFR_HANDLED;
+	    }
+	    break;
+	case 0x64:		/* fs: */
+	    if (_fs == 0) {
+		D_printf("MSDOS: client tries to use use gdt 0 as fs\n");
+		_fs = ConvertSegmentToDescriptor(0);
+		fixed = MFR_HANDLED;
+	    }
+	    break;
+	case 0x65:		/* gs: */
+	    if (_gs == 0) {
+		D_printf("MSDOS: client tries to use use gdt 0 as es\n");
+		_gs = ConvertSegmentToDescriptor(0);
+		fixed = MFR_HANDLED;
+	    }
+	    break;
+	case 0xf2:		/* REPNE prefix */
+	case 0xf3:		/* REP, REPE */
+	    /* this might be a string insn */
+	    switch (*(csp+1)) {
+	    case 0xaa: case 0xab:		/* stos */
+	    case 0xae: case 0xaf:	        /* scas */
+		/* only use es */
+		if (_es == 0) {
+		    D_printf("MSDOS: client tries to use use gdt 0 as es\n");
+		    _es = ConvertSegmentToDescriptor(0);
+		    fixed = MFR_HANDLED;
+		}
+		break;
+	    case 0xa4: case 0xa5:		/* movs */
+	    case 0xa6: case 0xa7:         /* cmps */
+		/* use both ds and es */
+		if (_es == 0) {
+		    D_printf("MSDOS: client tries to use use gdt 0 as es\n");
+		    _es = ConvertSegmentToDescriptor(0);
+		    fixed = MFR_HANDLED;
+		}
+		if (_ds == 0) {
+		    D_printf("MSDOS: client tries to use use gdt 0 as ds\n");
+		    _ds = ConvertSegmentToDescriptor(0);
+		    fixed = MFR_HANDLED;
+		}
+		break;
+	    }
+	    break;
+	case 0x3e:		/* ds: */
+	default:		/* assume default is using ds, but if the */
+				/* client sets ss to 0, it is totally broken */
+	    if (_ds == 0) {
+		D_printf("MSDOS: client tries to use use gdt 0 as ds\n");
+		_ds = ConvertSegmentToDescriptor(0);
+		fixed = MFR_HANDLED;
+	    }
+	    break;
+	}
+	return fixed;
+#else
 	return MFR_NOT_HANDLED;
+#endif
+    }
 
     /* now it is a invalid selector error, try to fix it if it is */
     /* caused by an instruction such as mov Sreg,r/m16            */
